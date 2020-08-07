68d99a38d208bd033001e5418a748d795d5372bf
pranaysaha <pranay@zestmoney.in>
2020-08-07 23:06:26 +0530

    variables concpet added

---
+++ variables/numeric_type.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // Implicitly defined integers
+    let x = 10;
+
+    //Explicitly defined integers
+    let a:i32 = 10;
+    let b:u64 = 20;
+    let c:usize = 30;
+    let d:isize = 40;
+
+    //print the values
+    println!("x: {}", x);
+    println!("a: {}", a);
+    println!("b: {}", b);
+    println!("c: {}", c);
+    println!("d: {}", d);
+}
